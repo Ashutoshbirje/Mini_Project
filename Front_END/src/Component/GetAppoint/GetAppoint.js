2a505689c449a2b934671c9c0e03982be7634594
Ashutoshbirje <ashutoshbirje4@gmail.com>
2024-10-17 04:41:21 +0530

    ok

--- Front_END/src/Component/GetAppoint/GetAppoint.js
+++ Front_END/src/Component/GetAppoint/GetAppoint.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './GetAppoint.css'; 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faKitMedical, faPhone, faShoppingCart } from '@fortawesome/free-solid-svg-icons'; 
 
 const GetAppoint = () => {
     return (
